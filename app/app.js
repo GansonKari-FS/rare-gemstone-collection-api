ad53b3dd4fc039b309715d44ba3707d9d86d7a55
GansonKari-FS <kDganson@student.fullsail.edu>
2026-04-28 15:46:24 -0500

    Added CORS support for React client

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const gemRoutes = require("../routes/gemRoutes");
 const certifiedGemstoneRoutes = require("../routes/certifiedGemstoneRoutes");
 const geoRoutes = require("../routes/geoRoutes");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
